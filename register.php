861745b3d757b01dc7a747bb7958965a510d4902
ddqq4 <dariab51261@gmail.com>
2025-12-27 02:35:05 +0300

    Update register.php

--- register.php
+++ register.php
@@ -121,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
         </div>
         <div class="footer-bottom">
-            <p>© 2024 Ароматный Мир. Все права защищены.</p>
+            <p>© 2025 Ароматный Мир. Все права защищены.</p>
         </div>
     </footer>
 </body>
